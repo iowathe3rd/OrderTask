d23911a237d9171dae72ec3b0d12cc4067f8b4cd
Baurzhan <iowathe3rd@proton.me>
2024-05-20 23:27:58 +0500

    Добавьте файлы проекта.

---
+++ Order/Models/Domain/Order.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Order.Models.Domain
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "The order name is required")]
+        public string OrderName { get; set; }
+
+        [Required(ErrorMessage = "The quantity is required")]
+        public int Quantity { get; set; }
+
+        [Required(ErrorMessage = "The unit price is required")]
+        public decimal UnitPrice { get; set; }
+    }
+}
