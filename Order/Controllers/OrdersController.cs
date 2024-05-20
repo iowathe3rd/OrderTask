8a091e7bedd1837939cfb87f30039c1bc422a1bd
Baurzhan <iowathe3rd@proton.me>
2024-05-21 00:05:28 +0500

    feat(logic): added controller with basic endpoints and service with logic

---
+++ Order/Controllers/OrdersController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Order.Data;
+using Order.Models.Domain;
+using Order.Services;
+
+namespace Order.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly IOrderService _orderService;
+
+        public OrdersController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllOrders()
+        {
+            var orders = _orderService.GetAllOrders();
+            return Ok(orders);
+        }
+
+        [HttpPost]
+        public IActionResult CreateOrder(Models.Domain.Order order)
+        {
+            _orderService.CreateOrder(order);
+            return CreatedAtAction(nameof(GetOrderById), new { id = order.Id }, order);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteOrder(Guid id)
+        {
+            _orderService.DeleteOrder(id);
+            return NoContent();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetOrderById(Guid id)
+        {
+            var order = _orderService.GetOrderById(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+            return Ok(order);
+        }
+    }
+}
---
+++ Order/Services/OrderService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Order.Data;
+using Order.Models.Domain;
+
+namespace Order.Services
+{
+    public interface IOrderService
+    {
+        IEnumerable<Models.Domain.Order> GetAllOrders();
+        Models.Domain.Order GetOrderById(Guid id);
+        void CreateOrder(Models.Domain.Order order);
+        void DeleteOrder(Guid id);
+    }
+
+   public class OrderService : IOrderService
+    {
+        private readonly OrderDbContext _dbContext;
+
+        public OrderService(OrderDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IEnumerable<Models.Domain.Order> GetAllOrders()
+        {
+            return _dbContext.Orders.ToList();
+        }
+
+        public Models.Domain.Order GetOrderById(Guid id)
+        {
+            return _dbContext.Orders.Find(id);
+        }
+
+        public void CreateOrder(Models.Domain.Order order)
+        {
+            order.Id = Guid.NewGuid();
+            _dbContext.Orders.Add(order);
+            _dbContext.SaveChanges();
+        }
+
+        public void DeleteOrder(Guid id)
+        {
+            var order = _dbContext.Orders.Find(id);
+            if (order != null)
+            {
+                _dbContext.Orders.Remove(order);
+                _dbContext.SaveChanges();
+            }
+        }
+    }
+}
