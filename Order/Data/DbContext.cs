89fb78cd376c8db726fef0fecfd742d9d675f1cd
Baurzhan <iowathe3rd@proton.me>
2024-05-20 23:45:09 +0500

    feat(database): added connection string, added db context file, modified Program.cs

---
+++ Order/Data/DbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Order.Models.Domain;
+
+namespace Order.Data
+{
+    public class OrderDbContext : DbContext
+    {
+        public OrderDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Order.Models.Domain.Order> Orders { get; set; }
+
+     }
+}
--- Order/Program.cs
+++ Order/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Order.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<OrderDbContext>(options =>
+    options.UseNpgsql(connectionString)
+);
 
 var app = builder.Build();
 
